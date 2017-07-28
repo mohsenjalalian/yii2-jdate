5c83801eb2ef9fe62e97d0e873bc198c70d5eeb6
mohsenjalalian <jalalianmohsen@gmail.com>
2017-07-28 17:22:18 +0430

    to gregorian

--- DateTime.php
+++ DateTime.php
@@ -26,7 +26,7 @@ class DateTime
 			$year	= $dateTime->format('Y'),
 			$month 	= $dateTime->format('m'),
 			$day 	= $dateTime->format('d'));
-		
+
 		$res = '';
 		$skipNextCahr = false;
 		for ($i = 0; $i < strlen($dateFormat); $i++) {
@@ -134,48 +134,105 @@ class DateTime
 		return $this->date($var);
 	}
 
-	function toJalaliDate($g_y, $g_m, $g_d) 
+	function toJalaliDate($g_y, $g_m, $g_d)
 	{
 
-		$g_days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31); 
-		$j_days_in_month = array(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29);      
-		$gy = $g_y-1600; 
-		$gm = $g_m-1; 
-		$gd = $g_d-1; 
-		$g_day_no = 365*$gy+$this->div($gy+3,4)-$this->div($gy+99,100)+$this->div($gy+399,400); 
-		for ($i=0; $i < $gm; ++$i) 
-			$g_day_no += $g_days_in_month[$i]; 
+		$g_days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+		$j_days_in_month = array(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29);
+		$gy = $g_y-1600;
+		$gm = $g_m-1;
+		$gd = $g_d-1;
+		$g_day_no = 365*$gy+$this->div($gy+3,4)-$this->div($gy+99,100)+$this->div($gy+399,400);
+		for ($i=0; $i < $gm; ++$i)
+			$g_day_no += $g_days_in_month[$i];
 
-		if ($gm>1 && (($gy%4==0 && $gy%100!=0) || ($gy%400==0))) 
-			/* leap and after Feb */ 
-			$g_day_no++; 
-		$g_day_no += $gd; 
-		$j_day_no = $g_day_no-79; 
-		$j_np = $this->div($j_day_no, 12053); /* 12053 = 365*33 + 32/4 */ 
-		$j_day_no = $j_day_no % 12053; 
-		$jy = 979+33*$j_np+4*$this->div($j_day_no,1461); /* 1461 = 365*4 + 4/4 */ 
-		$j_day_no %= 1461; 
-		if ($j_day_no >= 366) { 
+		if ($gm>1 && (($gy%4==0 && $gy%100!=0) || ($gy%400==0)))
+			/* leap and after Feb */
+			$g_day_no++;
+		$g_day_no += $gd;
+		$j_day_no = $g_day_no-79;
+		$j_np = $this->div($j_day_no, 12053); /* 12053 = 365*33 + 32/4 */
+		$j_day_no = $j_day_no % 12053;
+		$jy = 979+33*$j_np+4*$this->div($j_day_no,1461); /* 1461 = 365*4 + 4/4 */
+		$j_day_no %= 1461;
+		if ($j_day_no >= 366) {
 
-		  $jy += $this->div($j_day_no-1, 365); 
+		  $jy += $this->div($j_day_no-1, 365);
 
-		  $j_day_no = ($j_day_no-1)%365; 
+		  $j_day_no = ($j_day_no-1)%365;
 
-		} 
-		for ($i = 0; $i < 11 && $j_day_no >= $j_days_in_month[$i]; ++$i) 
-			$j_day_no -= $j_days_in_month[$i]; 
+		}
+		for ($i = 0; $i < 11 && $j_day_no >= $j_days_in_month[$i]; ++$i)
+			$j_day_no -= $j_days_in_month[$i];
 
 		$jm = $i+1;
-		$jd = $j_day_no+1; 
-		return array($jy, $jm, $jd); 
+		$jd = $j_day_no+1;
+		return array($jy, $jm, $jd);
 
 	}
+
+    public static function toGregorian($j_y, $j_m, $j_d)
+    {
+        $g_days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+        $j_days_in_month = array(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29);
+
+        $jy = $j_y-979;
+        $jm = $j_m-1;
+        $jd = $j_d-1;
+
+        $j_day_no = 365*$jy + self::divs($jy, 33)*8 + self::divs($jy%33+3, 4);
+        for ($i=0; $i < $jm; ++$i)
+            $j_day_no += $j_days_in_month[$i];
+
+        $j_day_no += $jd;
+
+        $g_day_no = $j_day_no+79;
+
+        $gy = 1600 + 400*self::divs($g_day_no, 146097);
+        $g_day_no = $g_day_no % 146097;
+
+        $leap = true;
+        if ($g_day_no >= 36525) {
+            $g_day_no--;
+            $gy += 100*self::divs($g_day_no,  36524);
+            $g_day_no = $g_day_no % 36524;
+
+            if ($g_day_no >= 365)
+                $g_day_no++;
+            else
+                $leap = false;
+        }
+
+        $gy += 4*self::divs($g_day_no, 1461);
+        $g_day_no %= 1461;
+
+        if ($g_day_no >= 366) {
+            $leap = false;
+
+            $g_day_no--;
+            $gy += self::divs($g_day_no, 365);
+            $g_day_no = $g_day_no % 365;
+        }
+
+        for ($i = 0; $g_day_no >= $g_days_in_month[$i] + ($i == 1 && $leap); $i++)
+            $g_day_no -= $g_days_in_month[$i] + ($i == 1 && $leap);
+        $gm = $i+1;
+        $gd = $g_day_no+1;
+
+        return array($gy, $gm, $gd);
+
+    }
 
 	function div($a,$b) {
 		return (int) ($a / $b);
 	}
 
-	function getDayNames()
+    private static function divs($a, $b)
+    {
+        return (int) ($a / $b);
+    }
+
+    function getDayNames()
 	{
 		return [
 			'saturday' => 'شنبه',
@@ -185,7 +242,7 @@ class DateTime
 			'wednesday' => 'چهارشنبه',
 			'thursday' => 'پنجشنبه',
 			'friday' => 'جمعه',
-			
+
 		];
 	}
 	function getDayShortNames()
